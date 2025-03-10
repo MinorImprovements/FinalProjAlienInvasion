c67f7d4bdea6463aef08869d5fd65e150760b0a4
Xavier Welker <fakenmae@gmail.com>
2025-03-09 20:57:19 -0700

    Added backround color and frame rate control to alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,8 +11,14 @@ class Alieninvasion:
         #Initialize the game and create game resources
         pygame.init()
 
+        #crate a instance of the Clock class to controll the frame rate
+        self.clock = pygame.time.Clock()
+
         self.screen = pygame.display.set_mode((1250,1080))
         pygame.display.set_caption("Alien Invasion")
+
+        #set the background color
+        self.bg_color = (230,230,230)
 
     def run_game(self):
         #Start the main loop for the game
@@ -22,8 +28,14 @@ class Alieninvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            #redraw the screen during each pass through the loop
+            self.screen.fill(self.bg_color)
+
             #Make the most recently drawn screen visible
             pygame.display.flip()
+
+            #set frames per second
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     #make game instance and run the game

5d1c5a4dedcde7babfbc293841db09a3fec6194d
Xavier Welker <fakenmae@gmail.com>
2025-03-09 20:50:00 -0700

    Created alien_invasion.py that contains AlienInvasion class

---
+++ alien_invasion.py
@@ -0,0 +1,31 @@
+#Xavier Welker 20250309
+#This file contains the logic for displaying screen, game staying on and getting some user inputs
+
+import sys
+import pygame
+
+class Alieninvasion:
+    #Overall class to manage game assets and behavior
+
+    def __init__(self):
+        #Initialize the game and create game resources
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1250,1080))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        #Start the main loop for the game
+        while True:
+            #Watch for keyboard and mouse events
+            for event in pygame.evet.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #Make the most recently drawn screen visible
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    #make game instance and run the game
+    ai = AlienInvasion()
+    ai.run_game()
